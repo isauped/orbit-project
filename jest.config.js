862771cae07ed4b4d61e6508b0b717ba16b1f58c
Bruno Pedrosa <bruno@biolumic.com>
2024-05-19 13:33:12 +1200

    chore(config): config the project with node and typescript

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/test/**/*.test.ts'],
+    moduleNameMapper: {
+      '^(\\.{1,2}/.*)\\.js$': '$1'
+    }
+  };
+  
